3229c06cfae287ecf166229aae2b47b3ee4cf121
Sarfaraz Ahmed Malik <115607089+SarfarazAhmed12@users.noreply.github.com>
2022-10-11 22:34:55 -0700

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+ class oddeven {
+   public static void main(String[] args){
+   
+   system.out.println("enter the number");
+   scanner obj =new scanner(system.in);
+   int a =obj.nextInt();
+   if(a%2==0)
+   {
+       system.out.println("number is even");
+       
+   }
+   else
+   {
+      system.out.println("number is odd");
+   
+   }
+   
+   }
+}
